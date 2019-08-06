d726f135b2a140d2cc74b7c8b3ba665a92bb3809
Dorota Gil <gildota@wp.pl>
2019-08-06 20:44:45 +0200

    Add constructor

--- BeerApp/src/app/beer/beer.component.ts
+++ BeerApp/src/app/beer/beer.component.ts
@@ -1,11 +1,13 @@
 import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
+import { BeerService } from '../_service/beer.service';
 @Component({
   selector: 'app-beer',
   templateUrl: './beer.component.html',
   styleUrls: ['./beer.component.css']
 })
 export class BeerComponent implements OnInit {
+  constructor(private beerService: BeerService) {
   ngOnInit() {
   }
 }
