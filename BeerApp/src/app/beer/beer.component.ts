b6e13e58cb17f5a2858743ef28d010d2ec732b02
Dorota Gil <gildota@wp.pl>
2019-08-06 20:27:34 +0200

    Add beer.component

---
+++ BeerApp/src/app/beer/beer.component.ts
@@ -0,0 +1,11 @@
+import { element } from 'protractor';
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-beer',
+  templateUrl: './beer.component.html',
+  styleUrls: ['./beer.component.css']
+})
+export class BeerComponent implements OnInit {
+  ngOnInit() {
+  }
+}
