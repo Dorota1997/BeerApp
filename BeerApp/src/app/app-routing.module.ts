ee57e0ddd93758f897aa3266a006c141fa7c6623
Dorota Gil <gildota@wp.pl>
2019-08-06 20:35:41 +0200

    Add BeerComponent to routes in app-routing.module

--- BeerApp/src/app/app-routing.module.ts
+++ BeerApp/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { BeerComponent } from './beer/beer.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'beer',
+    component: BeerComponent
+  },
+  {
+    path: '',
+    component: BeerComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
