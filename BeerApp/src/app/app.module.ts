c976b62a97cfbc257e1f877607fbf49835f2fc32
Dorota Gil <gildota@wp.pl>
2019-08-06 20:34:11 +0200

    Add beer.component to declarations in app.module

--- BeerApp/src/app/app.module.ts
+++ BeerApp/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgModule, Component } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BeerComponent } from './beer/beer.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -14,5 +17,18 @@ import { AppComponent } from './app.component';
   ],
   providers: [],
   bootstrap: [AppComponent]
+   exports: [
+   ],
+   declarations: [
+      AppComponent,
+      BeerComponent,
+   ],
+   imports: [
+      BrowserModule,
+   ],
+   providers: [{provide: APP_BASE_HREF, useValue: '/'}],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
